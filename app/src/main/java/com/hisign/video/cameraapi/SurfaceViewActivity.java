0b29c9f2f3af1e7b958918338ce57071211eb51f
dazhangyu212 <820312435@qq.com>
2018-05-25 19:01:56 +0800

    音频开发一部分内容:PCM的采集和PCM文件转为WAV,播放PCM

---
+++ app/src/main/java/com/hisign/video/cameraapi/SurfaceViewActivity.java
@@ -0,0 +1,19 @@
+package com.hisign.video.cameraapi;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 描述：用SurfaceView 预览Camera
+ * 公司：北京海鑫科金高科技股份有限公司
+ * 作者：zhangyu
+ * 创建时间 2018/5/25
+ */
+
+public class SurfaceViewActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
